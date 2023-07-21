e668f538df356f2b664ed8b1c82425aec65acedd
Nerma5 <nermahh5@gmail.com>
2023-07-21 14:49:44 +0200

    init

---
+++ src/components/CountryStats/CountryList.jsx
@@ -0,0 +1,304 @@
+/* eslint-disable react/prop-types */
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import{ useState } from "react";
+
+
+const countries = [
+    { name: 'Afghanistan', code: 'AF' },
+    { name: 'Albania', code: 'AL' },
+    { name: 'Algeria', code: 'DZ' },
+    { name: 'Andorra', code: 'AD' },
+    { name: 'Angola', code: 'AO' },
+    { name: 'Anguilla', code: 'AI' },
+    { name: 'Antigua-and-Barbuda', code: 'AG' },
+    { name: 'Argentina', code: 'AR' },
+    { name: 'Armenia', code: 'AM' },
+    { name: 'Aruba', code: 'AW' },
+    { name: 'Australia', code: 'AU' },
+    { name: 'Austria', code: 'AT' },
+    { name: 'Azerbaijan', code: 'AZ' },
+    { name: 'Bahamas', code: 'BS' },
+    { name: 'Bahrain', code: 'BH' },
+    { name: 'Bangladesh', code: 'BD' },
+    { name: 'Barbados', code: 'BB' },
+    { name: 'Belarus', code: 'BY' },
+    { name: 'Belgium', code: 'BE' },
+    { name: 'Belize', code: 'BZ' },
+    { name: 'Benin', code: 'BJ' },
+    { name: 'Bermuda', code: 'BM' },
+    { name: 'Bhutan', code: 'BT' },
+    { name: 'Bolivia', code: 'BO' },
+    { name: 'Bosnia-and-Herzegovina', code: 'BA' },
+    { name: 'Botswana', code: 'BW' },
+    { name: 'Brazil', code: 'BR' },
+    { name: 'British-Virgin-Islands', code: 'VG' },
+    { name: 'Brunei', code: 'BN' },
+    { name: 'Bulgaria', code: 'BG' },
+    { name: 'Burkina-Faso', code: 'BF' },
+    { name: 'Burundi', code: 'BI' },
+    { name: 'Cabo-Verde', code: 'CV' },
+    { name: 'Cambodia', code: 'KH' },
+    { name: 'Cameroon', code: 'CM' },
+    { name: 'Canada', code: 'CA' },
+    { name: 'CAR', code: null },
+    { name: 'Caribbean-Netherlands', code: null },
+    { name: 'Cayman-Islands', code: 'KY' },
+    { name: 'Chad', code: 'TD' },
+    { name: 'Channel-Islands', code: null },
+    { name: 'Chile', code: 'CL' },
+    { name: 'China', code: 'CN' },
+    { name: 'Colombia', code: 'CO' },
+    { name: 'Comoros', code: 'KM' },
+    { name: 'Congo', code: 'CG' },
+    { name: 'Cook-Islands', code: 'CK' },
+    { name: 'Costa-Rica', code: 'CR' },
+    { name: 'Croatia', code: 'HR' },
+    { name: 'Cuba', code: 'CU' },
+    { name: 'Cura&ccedil;ao', code: 'CW' },
+    { name: 'Cyprus', code: 'CY' },
+    { name: 'Czechia', code: 'CZ' },
+    { name: 'Denmark', code: 'DK' },
+    { name: 'Diamond-Princess', code: null },
+    { name: 'Djibouti', code: 'DJ' },
+    { name: 'Dominica', code: 'DM' },
+    { name: 'Dominican-Republic', code: 'DO' },
+    { name: 'DPRK', code: null },
+    { name: 'DRC', code: null },
+    { name: 'Ecuador', code: 'EC' },
+    { name: 'Egypt', code: 'EG' },
+    { name: 'El-Salvador', code: 'SV' },
+    { name: 'Equatorial-Guinea', code: 'GQ' },
+    { name: 'Eritrea', code: 'ER' },
+    { name: 'Estonia', code: 'EE' },
+    { name: 'Eswatini', code: 'SZ' },
+    { name: 'Ethiopia', code: 'ET' },
+    { name: 'Faeroe-Islands', code: 'FO' },
+    { name: 'Falkland-Islands', code: 'FK' },
+    { name: 'Fiji', code: 'FJ' },
+    { name: 'Finland', code: 'FI' },
+    { name: 'France', code: 'FR' },
+    { name: 'French-Guiana', code: 'GF' },
+    { name: 'French-Polynesia', code: 'PF' },
+    { name: 'Gabon', code: 'GA' },
+    { name: 'Gambia', code: 'GM' },
+    { name: 'Georgia', code: 'GE' },
+    { name: 'Germany', code: 'DE' },
+    { name: 'Ghana', code: 'GH' },
+    { name: 'Gibraltar', code: 'GI' },
+    { name: 'Greece', code: 'GR' },
+    { name: 'Greenland', code: 'GL' },
+    { name: 'Grenada', code: 'GD' },
+    { name: 'Guadeloupe', code: 'GP' },
+    { name: 'Guatemala', code: 'GT' },
+    { name: 'Guinea', code: 'GN' },
+    { name: 'Guinea-Bissau', code: 'GW' },
+    { name: 'Guyana', code: 'GY' },
+    { name: 'Haiti', code: 'HT' },
+    { name: 'Honduras', code: 'HN' },
+    { name: 'Hong-Kong', code: 'HK' },
+    { name: 'Hungary', code: 'HU' },
+    { name: 'Iceland', code: 'IS' },
+    { name: 'India', code: 'IN' },
+    { name: 'Indonesia', code: 'ID' },
+    { name: 'Iran', code: 'IR' },
+    { name: 'Iraq', code: 'IQ' },
+    { name: 'Ireland', code: 'IE' },
+    { name: 'Isle-of-Man', code: 'IM' },
+    { name: 'Israel', code: 'IL' },
+    { name: 'Italy', code: 'IT' },
+    { name: 'Ivory-Coast', code: 'CI' },
+    { name: 'Jamaica', code: 'JM' },
+    { name: 'Japan', code: 'JP' },
+    { name: 'Jordan', code: 'JO' },
+    { name: 'Kazakhstan', code: 'KZ' },
+    { name: 'Kenya', code: 'KE' },
+    { name: 'Kiribati', code: 'KI' },
+    { name: 'Kuwait', code: 'KW' },
+    { name: 'Kyrgyzstan', code: 'KG' },
+    { name: 'Laos', code: 'LA' },
+    { name: 'Latvia', code: 'LV' },
+    { name: 'Lebanon', code: 'LB' },
+    { name: 'Lesotho', code: 'LS' },
+    { name: 'Liberia', code: 'LR' },
+    { name: 'Libya', code: 'LY' },
+    { name: 'Liechtenstein', code: 'LI' },
+    { name: 'Lithuania', code: 'LT' },
+    { name: 'Luxembourg', code: 'LU' },
+    { name: 'Macao', code: 'MO' },
+    { name: 'Madagascar', code: 'MG' },
+    { name: 'Malawi', code: 'MW' },
+    { name: 'Malaysia', code: 'MY' },
+    { name: 'Maldives', code: 'MV' },
+    { name: 'Mali', code: 'ML' },
+    { name: 'Malta', code: 'MT' },
+    { name: 'Marshall-Islands', code: 'MH' },
+    { name: 'Martinique', code: 'MQ' },
+    { name: 'Mauritania', code: 'MR' },
+    { name: 'Mauritius', code: 'MU' },
+    { name: 'Mayotte', code: 'YT' },
+    { name: 'Mexico', code: 'MX' },
+    { name: 'Micronesia', code: 'FM' },
+    { name: 'Moldova', code: 'MD' },
+    { name: 'Monaco', code: 'MC' },
+    { name: 'Mongolia', code: 'MN' },
+    { name: 'Montenegro', code: 'ME' },
+    { name: 'Montserrat', code: 'MS' },
+    { name: 'Morocco', code: 'MA' },
+    { name: 'Mozambique', code: 'MZ' },
+    { name: 'MS-Zaandam', code: null },
+    { name: 'Myanmar', code: 'MM' },
+    { name: 'Namibia', code: 'NA' },
+    { name: 'Nauru', code: 'NR' },
+    { name: 'Nepal', code: 'NP' },
+    { name: 'Netherlands', code: 'NL' },
+    { name: 'New-Caledonia', code: 'NC' },
+    { name: 'New-Zealand', code: 'NZ' },
+    { name: 'Nicaragua', code: 'NI' },
+    { name: 'Niger', code: 'NE' },
+    { name: 'Nigeria', code: 'NG' },
+    { name: 'Niue', code: 'NU' },
+    { name: 'North-Macedonia', code: 'MK' },
+    { name: 'Norway', code: 'NO' },
+    { name: 'Oman', code: 'OM' },
+    { name: 'Pakistan', code: 'PK' },
+    { name: 'Palau', code: 'PW' },
+    { name: 'Palestine', code: 'PS' },
+    { name: 'Panama', code: 'PA' },
+    { name: 'Papua-New-Guinea', code: 'PG' },
+    { name: 'Paraguay', code: 'PY' },
+    { name: 'Peru', code: 'PE' },
+    { name: 'Philippines', code: 'PH' },
+    { name: 'Poland', code: 'PL' },
+    { name: 'Portugal', code: 'PT' },
+    { name: 'Qatar', code: 'QA' },
+    { name: 'R&eacute;union', code: 'RE' },
+    { name: 'Romania', code: 'RO' },
+    { name: 'Russia', code: 'RU' },
+    { name: 'Rwanda', code: 'RW' },
+    { name: 'S-Korea', code: null },
+    { name: 'Saint-Helena', code: 'SH' },
+    { name: 'Saint-Kitts-and-Nevis', code: 'KN' },
+    { name: 'Saint-Lucia', code: 'LC' },
+    { name: 'Saint-Martin', code: 'MF' },
+    { name: 'Saint-Pierre-Miquelon', code: 'PM' },
+    { name: 'Samoa', code: 'WS' },
+    { name: 'San-Marino', code: 'SM' },
+    { name: 'Sao-Tome-and-Principe', code: 'ST' },
+    { name: 'Saudi-Arabia', code: 'SA' },
+    { name: 'Senegal', code: 'SN' },
+    { name: 'Serbia', code: 'RS' },
+    { name: 'Seychelles', code: 'SC' },
+    { name: 'Sierra-Leone', code: 'SL' },
+    { name: 'Singapore', code: 'SG' },
+    { name: 'Sint-Maarten', code: 'SX' },
+    { name: 'Slovakia', code: 'SK' },
+    { name: 'Slovenia', code: 'SI' },
+    { name: 'Solomon-Islands', code: 'SB' },
+    { name: 'Somalia', code: 'SO' },
+    { name: 'South-Africa', code: 'ZA' },
+    { name: 'South-Sudan', code: 'SS' },
+    { name: 'Spain', code: 'ES' },
+    { name: 'Sri-Lanka', code: 'LK' },
+    { name: 'St-Barth', code: 'BL' },
+    { name: 'St-Vincent-Grenadines', code: 'VC' },
+    { name: 'Sudan', code: 'SD' },
+    { name: 'Suriname', code: 'SR' },
+    { name: 'Sweden', code: 'SE' },
+    { name: 'Switzerland', code: 'CH' },
+    { name: 'Syria', code: 'SY' },
+    { name: 'Taiwan', code: 'TW' },
+    { name: 'Tajikistan', code: 'TJ' },
+    { name: 'Tanzania', code: 'TZ' },
+    { name: 'Thailand', code: 'TH' },
+    { name: 'Timor-Leste', code: 'TL' },
+    { name: 'Togo', code: 'TG' },
+    { name: 'Tokelau', code: 'TK' },
+    { name: 'Tonga', code: 'TO' },
+    { name: 'Trinidad-and-Tobago', code: 'TT' },
+    { name: 'Tunisia', code: 'TN' },
+    { name: 'Turkey', code: 'TR' },
+    { name: 'Turks-and-Caicos', code: 'TC' },
+    { name: 'Tuvalu', code: 'TV' },
+    { name: 'UAE', code: 'AE' },
+    { name: 'Uganda', code: 'UG' },
+    { name: 'UK', code: 'GB' },
+    { name: 'Ukraine', code: 'UA' },
+    { name: 'Uruguay', code: 'UY' },
+    { name: 'USA', code: 'US' },
+    { name: 'Uzbekistan', code: 'UZ' },
+    { name: 'Vanuatu', code: 'VU' },
+    { name: 'Vatican-City', code: 'VA' },
+    { name: 'Venezuela', code: 'VE' },
+    { name: 'Vietnam', code: 'VN' },
+    { name: 'Wallis-and-Futuna', code: 'WF' },
+    { name: 'Western-Sahara', code: 'EH' },
+    { name: 'Yemen', code: 'YE' },
+    { name: 'Zambia', code: 'ZM' },
+    { name: 'Zimbabwe', code: 'ZW' },
+  ];
+
+
+const CountrySelect = () => {
+  const [selectedCountry, setSelectedCountry] = useState({
+    name: "Serbia",
+    code: "RS",
+  });
+
+  const handleCountryChange = (event, value) => {
+    if (value) {
+      setSelectedCountry({
+        name: value.name,
+        code: value.code ? value.code.toLowerCase() : "",
+      });
+    }
+  };
+
+  return (
+    <Autocomplete
+      onChange={handleCountryChange}
+      value={selectedCountry}
+      id="country-select-demo"
+      sx={{
+        width: 300,
+      }}
+      options={countries}
+      autoHighlight
+      getOptionLabel={(option) => option.name}
+      renderOption={(props, option) => (
+        <Box
+          component="li"
+          sx={{
+            "& > img": { mr: 2, flexShrink: 0 },
+          }}
+          {...props}
+        >
+          <img
+            loading="lazy"
+            width="20"
+            src={`https://flagcdn.com/w20/${option.code ? option.code.toLowerCase() : ""}.png`}
+            srcSet={`https://flagcdn.com/w40/${option.code ? option.code.toLowerCase() : ""}.png 2x`}
+            alt=""
+          />
+          {option.name} ({option.code})
+        </Box>
+      )}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Choose a country"
+          inputProps={{
+            ...params.inputProps,
+            autoComplete: "new-password", 
+          }}
+          sx={{
+            backgroundColor: "white",
+          }}
+        />
+      )}
+    />
+  );
+};
+
+export default CountrySelect;
---
+++ src/components/CountryStats/CountryStats.jsx
@@ -0,0 +1,48 @@
+import{ useState, useEffect } from "react";
+import axios from "axios";
+import CountryList from "./CountryList";
+import StateCard from "./CountryStatsCard";
+
+const CountryStats = () => {
+  const [selectedCountry, setSelectedCountry] = useState({ name: "Serbia", code: "RS" });
+  const [countryStats, setCountryStats] = useState(null);
+
+  const handleCountrySelect = (countryName, countryCode) => {
+    setSelectedCountry({ name: countryName, code: countryCode });
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://covid-193.p.rapidapi.com/history/${selectedCountry.code}`    
+        );
+        setCountryStats(response.data);
+      } catch (error) {
+        console.error("Error fetching country statistics:", error);
+      }
+    };
+
+    fetchData();
+  }, [selectedCountry.code]);
+
+  return (
+    <div>
+      <CountryList countryNameFunc={handleCountrySelect} />
+      {countryStats ? (
+        <StateCard
+          countryName={selectedCountry.name}
+          countryImg={selectedCountry.code}
+          new_cases={countryStats.new_cases}
+          new_death={countryStats.new_death}
+          new_recovered={countryStats.new_recovered}
+          total_active={countryStats.total_active}
+        />
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </div>
+  );
+};
+
+export default CountryStats;
